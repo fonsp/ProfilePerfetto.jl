b4638089e3a57541032fe2e5d7855b450241551d
Fons van der Plas <fonsvdplas@gmail.com>
2026-04-29 14:14:18 +0200

    Autocalibrate

--- src/ProfilePerfetto.jl
+++ src/ProfilePerfetto.jl
@@ -1,6 +1,7 @@
 module ProfilePerfetto
 
-export @perfetto, @perfetto_open, @perfetto_view, perfetto_view, perfetto_open
+export @perfetto, @perfetto_open, @perfetto_view, perfetto_view, perfetto_open,
+    @autoperfetto, @autoperfetto_open, @autoperfetto_view
 
 import Profile
 import JSON
@@ -13,5 +14,6 @@ include("json.jl")
 include("display.jl")
 include("view.jl")
 include("macro.jl")
+include("autocalibrate.jl")
 
 end # module
